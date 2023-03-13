962639247a7ce1d18c29ac71c47436eaceac1167
grasek <121408737+grasek@users.noreply.github.com>
2023-03-13 17:01:18 +0100

    Create factorize2.js

---
+++ Algorithms/Numbers/factorize2.js
@@ -0,0 +1,18 @@
+// That's more accurate alternative of factorize
+function factorize(num) {
+    const factors = [];
+    let divisor = 2;
+
+    while (num > 1) {
+      while (num % divisor === 0) {
+        factors.push(divisor);
+        num /= divisor;
+      }
+      divisor++;
+      if (divisor * divisor > num) {
+        if (num > 1) factors.push(num);
+        break;
+      }
+    }
+    return factors;
+  }
