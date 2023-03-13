0bc0d7eb366d16db85673bf73296ed607f8a81eb
grasek <121408737+grasek@users.noreply.github.com>
2023-03-13 17:06:30 +0100

    Update factorize.js

--- Algorithms/Numbers/factorize.js
+++ Algorithms/Numbers/factorize.js
@@ -1,3 +1,6 @@
+// It's worth noting that this way of factorize is quite efficient, but not always give exactly result,
+// for example it may returns "21" as a factors instead of "3, 7"
+// If you need more accurate alternative take a look at "factorize2"
 const factorize = (n) => {
   if (n < 2) return [];
   const quadraticFormula = (x) => (x * x + 1) % n;
